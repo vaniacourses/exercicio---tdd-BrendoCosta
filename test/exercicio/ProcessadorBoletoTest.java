0941ca137781d85e140ec9dcd1fb54bfe0caa324
Brendo Costa <brendocosta@id.uff.br>
2023-09-04 11:18:03 -0300

    Adicionado teste para pagamento fatura

--- test/exercicio/ProcessadorBoletoTest.java
+++ test/exercicio/ProcessadorBoletoTest.java
@@ -28,5 +28,22 @@ public class ProcessadorBoletoTest {
         Assertions.assertEquals(3, listaPagamentos.size());
 
     }
+    @DisplayName("Testa se a fatura foi paga")
+    @Test
+    public void testPagamentoFatura() {
+
+        ProcessadorBoleto processador = new ProcessadorBoleto();
+
+        Fatura fatura = new Fatura(new Date(), 1500.00, "Fulano");
+        List<Boleto> listaBoletos = Arrays.asList(
+            new Boleto(1, new Date(), 500.00),
+            new Boleto(2, new Date(), 400.00),
+            new Boleto(3, new Date(), 600.00)
+        );
+
+        List<Pagamento> listaPagamentos = processador.processar(listaBoletos, fatura);
+        Assertions.assertTrue(fatura.getStatusPaga());
+
+    }
 
 }
