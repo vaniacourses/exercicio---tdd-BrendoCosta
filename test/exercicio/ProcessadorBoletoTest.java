1d9d6220b6cea845e6f7048e501d6804f0bcb8ac
Brendo Costa <brendocosta@id.uff.br>
2023-09-04 10:56:05 -0300

    Adicionado teste para criação de pagamentos

---
+++ test/exercicio/ProcessadorBoletoTest.java
@@ -0,0 +1,32 @@
+package exercicio;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ProcessadorBoletoTest {
+
+    @DisplayName("Testa criação de pagamentos")
+    @Test
+    public void testCriacaoPagamentos() {
+
+        ProcessadorBoleto processador = new ProcessadorBoleto();
+
+        Fatura fatura = new Fatura(new Date(), 1500.00, "Fulano");
+        List<Boleto> listaBoletos = Arrays.asList(
+            new Boleto(1, new Date(), 500.00),
+            new Boleto(2, new Date(), 400.00),
+            new Boleto(3, new Date(), 600.00)
+        );
+
+        List<Pagamento> listaPagamentos = processador.processar(listaBoletos, fatura);
+        Assertions.assertEquals(3, listaPagamentos.size());
+
+    }
+
+}
